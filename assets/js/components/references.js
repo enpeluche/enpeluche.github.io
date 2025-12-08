1dd6489f98c8af53209e4da3112f7ecc5af733ee
Lucas Petit <lucas.noirotorion@gmail.com>
2025-12-08 19:38:22 +0100

    systeme de ref mis ầ jour plus qu a le met't're part'out'

--- assets/js/components/references.js
+++ assets/js/components/references.js
@@ -13,17 +13,98 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+async function loadReferences() {
+  try {
+    const response = await fetch('references.json');
+    const references = await response.json();
 
-document.addEventListener("DOMContentLoaded", () => {
-   const refs = document.querySelectorAll('a[class^=cite]');
-   
-   refs.forEach(a => {
-    a.innerHTML = `
-        <span class="author"></span>
-        <strong></strong>
-    `;
-   });
-});
+    //on recup toutes les refs de l article
+    const refs = document.querySelectorAll('a[class^=ref-]');
+
+    const refList = [];
+
+
+
+    refs.forEach(ref => {
+
+      const elt = references.find(REF => REF.id === ref.className);
+      
+      console.log(elt.title); //on pourrait' aussi ajout'er cet' elt' dans la list'e des references a la fin
+      refList.push(elt);
+
+      ref.title=elt.title;
+      ref.href="#"+ref.className;
+
+      const spanAuthor = document.createElement('span');
+      spanAuthor.className="author";
+      spanAuthor.textContent=elt.authors[0].given+elt.authors[0].family;
+
+      const year = document.createElement('strong');
+      year.textContent = elt.year;
+
+
+      ref.appendChild(spanAuthor);
+      ref.appendChild(year);
+
+      ref.className="cite " + ref.className;
+      
+    })
+
+
+
+      // on fait maintenant la partie reference du bas
+
+      const container = document.getElementById('main-references');
+
+const ol = document.createElement('ol');
+ol.className = "references";
+
+      refList.forEach(rr => {
+
+        
+
+        const li = document.createElement('li');
+
+        const div = document.createElement('div');
+
+        const spanAuthor = document.createElement('span');
+      spanAuthor.className="author";
+      spanAuthor.textContent=rr.authors[0].given+rr.authors[0].family;
+
+      const year = document.createElement('strong');
+      year.textContent = rr.year;
+
+
+      div.appendChild(spanAuthor);
+      div.appendChild(year);
+
+      li.appendChild(div);
+
+      const aa = document.createElement('a');
+      aa.title = "Accèder a l'article";
+      aa.href = rr.url;
+      aa.textContent = rr.title;
+
+      li.appendChild(aa);
+
+      li.id=rr.id;
+      ol.appendChild(li);
+
+
+      const h1 = document.createElement('h1');
+      h1.textContent = "Références";
+      
+      container.appendChild(h1);
+
+      container.appendChild(ol);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+document.addEventListener("DOMContentLoaded", loadReferences);
 
 //<a href="#ref-hales-2015" class="cite hales-2015">
 //  <span class="author"></span><strong></strong>
