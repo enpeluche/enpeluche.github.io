4500b472ce687ed962eb115da0b1dfabb7c167c5
Lucas Petit <lucas.noirotorion@gmail.com>
2025-12-12 15:25:13 +0100

    12 decc

--- assets/js/katex-config.js
+++ assets/js/katex-config.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     renderMathInElement(document.body, {
         delimiters: [
+        {left: "\\begin{equation}", right: "\\end{equation}", display: true},
+        {left: "\\begin{align}", right: "\\end{align}", display: true},
         {left: "$$", right: "$$", display: true},
         {left: "$", right: "$", display: false},
         {left: "\\[", right: "\\]", display:true},
