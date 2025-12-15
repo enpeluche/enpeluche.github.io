324b25ca498da40c9c55d4984b9aeabbabceb576
Lucas Petit <lucas.noirotorion@gmail.com>
2025-12-15 15:00:30 +0100

    Je veu en finir avec ces travaux academiques

--- assets/js/lib/katex-config.js
+++ assets/js/lib/katex-config.js
@@ -16,7 +16,9 @@ export function setupKatex() {
             "\\nint": "\\left\\lceil #1 \\right\\rfloor",
             "\\ps": "\\left\\langle #1 , #2 \\right\\rangle",
             "\\norm": "\\left\\| #1 \\right\\|",
-            "\\gscoeff": "\\frac{\\ps{#1}{#2}}{\\norm{#2}^2} #2"
+            "\\gscoeff": "\\frac{\\ps{#1}{#2}}{\\norm{#2}^2} #2",
+            "\\OO": "\\mathcal{O}",
+            "\\ra": "\\rightarrow"
         }
     });
 }    
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,14 @@ import { loadTableOfContents } from './components/toc.js';
 import { setupKatex } from './lib/katex-config.js';
 import { loadFooter } from './components/footer.js';
 import { loadReferences } from './pages/references.js';
+import { makeAlgorithm } from './pages/algorithm.js';
+import { makeArticle } from './pages/article.js';
 
 document.addEventListener("DOMContentLoaded", () => {
     setupKatex();
     loadTableOfContents(); 
     loadFooter();
     loadReferences();
+    makeAlgorithm();    
+    makeArticle();
 });
