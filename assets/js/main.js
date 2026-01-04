27a267af02702b15601dadea9f82b4d8c15f7df2
Lucas Petit <lucas.noirotorion@gmail.com>
2026-01-04 11:43:26 +0100

    refactor(news) : centralize js and css in components directory
    - Move news.js and news.css to /components/News/
    - Update imports in main.js and main.css to match new path

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 import { loadTableOfContents } from "/components/TableOfContents/TableOfContents.js";
 import { initScrollDown } from "/components/ScrollDown/ScrollDown.js";
 import { initCarousel } from "/components/Carousel/Carousel.js";
+import { createNews } from "/components/News/News.js";
 
 import { setupKatex } from "./lib/katex-config.js";
 import { loadFooter } from "./components/footer.js";
@@ -8,7 +9,6 @@ import { loadReferences } from "./pages/references.js";
 import { makeAlgorithm } from "./pages/algorithm.js";
 import { makeArticle } from "./pages/article.js";
 import { loadHeader } from "./components/header.js";
-import { createNews } from "./news.js";
 
 import { createProject } from "./project.js";
 import { initParticles } from "./particles.js";
